a6ce50903b87e171f60c57b551f11c7c52236432
zeroqs <94167692+zeroqs@users.noreply.github.com>
2022-04-15 20:01:11 +0300

    all done

--- files/functions.js
+++ files/functions.js
@@ -1,4 +1,3 @@
-// меню бургер
 export function menuBurger () { 
     const burgerMenu = document.querySelector('.menu__icon'); 
     if (burgerMenu) { 
@@ -10,3 +9,45 @@ export function menuBurger () {
         })   
     }
 }
+export function slider () {
+    const carouselSlide = document.querySelector(".carousel-slide");
+    const carouselImages = document.querySelectorAll(".carousel-slide img");
+    
+
+    // buttons
+    const nextButton = document.querySelector("#nextBtn");
+    const prevButton = document.querySelector("#prevBtn");
+
+    //counter
+    let counter = 1; // first image start
+    const size = carouselImages[counter].clientWidth; // дистанция перемещения одного слайда  
+    carouselSlide.style.transform = `translateX(${-size * counter}px)`;
+
+    nextButton.addEventListener("click", () => {
+        if (counter >= carouselImages.length - 1) return;
+        carouselSlide.style.transition = "transform 0.4s ease-in-out";
+        counter++;
+        carouselSlide.style.transform = `translateX(${-size * counter}px)`;
+    });
+    prevButton.addEventListener("click", () => {
+        if (counter <= 0) return; 
+        carouselSlide.style.transition = "transform 0.4s ease-in-out";
+        counter--;
+        carouselSlide.style.transform = `translateX(${-size * counter}px)`;
+    });
+    
+    carouselSlide.addEventListener('transitionend', ()=> {
+        if(carouselImages[counter].id ==='lastClone') {
+            carouselSlide.style.transition = "none";
+            counter = carouselImages.length - 2;
+            carouselSlide.style.transform = `translateX(${-size * counter}px)`;
+        }
+        if(carouselImages[counter].id ==='firstClone') {
+            carouselSlide.style.transition = "none";
+            counter = carouselImages.length - counter;
+            carouselSlide.style.transform = `translateX(${-size * counter}px)`;
+        }
+    })
+}
+
+
